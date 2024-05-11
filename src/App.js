ba8b8d5d3bdacc09d1bb024ce42c1531a09b5947
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-05-11 10:31:13 +0300

    new react Dj-Adv-1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js2 </code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -15,6 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
+          <p>Saber Mharesh</p>
           Learn React
         </a>
       </header>
